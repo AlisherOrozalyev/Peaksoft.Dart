e2f89d0db88f9eb45765ffedcc37bb3c7247f3ae
Alisher <alish.oroz@mail.ru>
2024-04-25 20:45:55 +0600

    not done

---
+++ kaitiloo_sabak/bin/kaitiloo_sabak.dart
@@ -0,0 +1,113 @@
+
+
+
+// void main() {
+// String text = 'Name';
+// int a = 1;
+// bool b = true;
+// double c = 1.2;
+// dynamic d = false;
+
+
+
+// print('text');
+// print(a);
+// print(b);
+// print(c);
+// print(d);
+// }
+
+
+// void main(){
+//   int result = num(5,5);
+//   print (result);
+// }
+
+// int num (int a, int b){
+//   return a+b;
+// }
+
+// void main(){
+// int result = nom(3,2);
+// print(result);
+// }
+// int nom(int a,int b){
+// return a*b;
+// }
+
+// void main(){
+// int result = aa(5,3,8,9);
+// print(result);
+// }
+
+// int aa(int a,int b,int c,int d){
+//   return a*b+c-d;
+// }
+
+// void main(){
+// String result = bb('Salam Aleykum');
+// print(result);
+// }
+// String bb(String a){
+//   return a;
+// }
+
+
+// void main(){
+//   String text = 'Salam';
+//   int a = 28;
+//   bool b = true;
+//   dynamic c = false;
+//   double d = 55.3;
+
+//   print(text);
+//   print(a);
+//   print(b);
+//   print(c);
+//   print(d);
+// }
+void main(){
+  String text = 'salam';
+  String text1 = 'hello';
+  String text2 = 'privet';
+
+  int a = 2;
+  int a1 = 3;
+  int a2 = 4;
+
+  bool b = true;
+  bool b1 = false;
+  bool b2 = true;
+
+  double c = 1.1;
+  double c1 = 1.2;
+  double c2 = 1.3;
+
+  dynamic d = 11;
+  dynamic d1 = false;
+  dynamic d2 = 1.1;
+  dynamic d3 = 'zdorovo';
+
+  
+print(text);
+print(text1);
+print(text2);
+
+print(a);
+print(a1);
+print(a2);
+
+print(b);
+print(b1);
+print(b2);
+
+print(c);
+print(c1);
+print(c2);
+
+print(d);
+print(d1);
+print(d2);
+print(d3);
+
+}
---
+++ kaitiloo_sabak/test/kaitiloo_sabak_test.dart
@@ -0,0 +1,8 @@
+import 'package:kaitiloo_sabak/kaitiloo_sabak.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
