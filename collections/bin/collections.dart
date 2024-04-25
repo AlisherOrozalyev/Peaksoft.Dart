ed8c72e3e11439ac81ffe8ced5b55137e974f8a1
Alisher <alish.oroz@mail.ru>
2024-04-25 23:08:08 +0600

    done

--- collections/bin/collections.dart
+++ collections/bin/collections.dart
@@ -1,12 +1,34 @@
-
-
-void main(List<String> arguments) {
+void main() {
 // int a = 1;
 // int b = 2;
 // int c = 3;
 
-// cllections
-// List
-List numbers = [1, 2, 3, 100, 5, 6];
-print(numbers);
+// cllections and Generic
+
+
+// List Бул collections
+// List<String> Бул деген Generic
+// List<String> listNames = ['Asan',
+// 'Uson',
+// 'Azat',
+// 'Bakyt',
+// 'Asan'];
+// print(listNames);
+
+// Set Бул Collection
+// Set<int> Бул Generic
+// Set<int> setName = {5, 
+// 6,
+// 2,
+// 8};
+// print(setName);
+
+// Map Бул Collection
+// Map<dynamic,String> Бул Generic
+  Map<dynamic,String> mapName = {
+    '1': 'Asan',
+     '2': 'Uson',
+      '3': 'Azat',
+       '4': 'Bakyt'};
+  print(mapName['2']);
 }
