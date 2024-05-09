ba917c35a8465add472f9a0d64f42ecff70a25fe
Alisher <alish.oroz@mail.ru>
2024-05-09 21:50:15 +0600

    done

--- collections/bin/collections.dart
+++ collections/bin/collections.dart
@@ -1,10 +1,9 @@
-void main() {
+// void main() {
 // int a = 1;
 // int b = 2;
 // int c = 3;
 
 // cllections and Generic
-
 
 // List Бул collections
 // List<String> Бул деген Generic
@@ -17,7 +16,7 @@ void main() {
 
 // Set Бул Collection
 // Set<int> Бул Generic
-// Set<int> setName = {5, 
+// Set<int> setName = {5,
 // 6,
 // 2,
 // 8};
@@ -32,11 +31,52 @@ void main() {
 //        '4': 'Bakyt'};
 //   print(mapName['4']);
 
-List<double> Bolchok = [
-  1.2,
-  22.1,
-  32.2,
-  55.43
-];
-print(Bolchok);
+// List<double> Bolchok = [
+//   1.2,
+//   22.1,
+//   32.2,
+//   55.43
+// ];
+// print(Bolchok);
+
+  // List<String> Names = ["Ali, Xan, Arman, Kojomkul"];
+  // print(Names);
+
+  // List<String> Aty = [
+  //   "Man, Woman, Men, Women"
+  // ];
+  // print(Aty);
+
+
+  // List<int> jashAt = [
+  //   2, 44, 12, 88
+  // ];
+  // print(jashAt);
+
+
+  // List<int> Jyl = [
+  //   1999, 2000, 2001, 2002
+  // ];
+  // print(Jyl);
+
+
+
+  // Set<int> gody = {
+  //   2020, 1212, 2322, 1313213
+  // };
+  // print(gody);
+// }
+
+void main(){
+
+  // Map<int,String> mepName = {
+  //   1: 'Ali', 2:'Xan', 3: 'Arman', 4: 'Sanya'
+  // };
+  // print(mepName[2]);
+
+Map<double,String> bolcokAt = {
+  2.2:'ekieki', 3.3: 'uchuch', 4.4: 'tirttort',1.1: 'birbir'
+};
+print(bolcokAt[2]);
+
 }
